5c9161c3b2f972e9ed0691c6f563c9155b39b025
Alejandro Belo <51330159+alejandrobelo32@users.noreply.github.com>
2021-12-25 12:56:54 -0300

    boton y pagina eliminar

--- borrar.php
+++ borrar.php
@@ -4,9 +4,13 @@ session_start();
 
 //echo $_COOKIE['cookie'];
 
-require_once "assets/php/validar_sesion.php";
+if(!isset($_SESSION["usuario"])){
 
-$_SESSION["paginaAnterior"]='noticias.php';
+	header("Location: ".$_SESSION["paginaAnterior"]);
+
+  //exit();
+
+}
 
 //include 'assets/php/conexion.php';
 
@@ -53,8 +57,7 @@ $_SESSION["paginaAnterior"]='noticias.php';
               </div>
             </li>
             <li class="main-header__nav-item"><a href="index.php" class="main-header__nav-link">INICIO</a></li>
-            <li class="main-header__nav-item"><a href="noticias.php" class="main-header__nav-link"
-                aria-current="page">NOTICIAS</a></li>
+            <li class="main-header__nav-item"><a href="noticias.php" class="main-header__nav-link">NOTICIAS</a></li>
             <li class="main-header__nav-item"><a href="#0" class="main-header__nav-link">STREAMING</a></li>
             <li class="main-header__nav-item"><a href="ofertas.php" class="main-header__nav-link">OFERTAS</a></li>
             <li class="main-header__nav-item main-header__nav-divider" aria-hidden="true"></li>
@@ -151,7 +154,7 @@ $_SESSION["paginaAnterior"]='noticias.php';
 
   $cantidad = 3;
 
-  $consultaNoticias="SELECT * FROM noticias ORDER BY fecha DESC LIMIT $cantidad OFFSET ".($pagina*$cantidad-$cantidad)."";
+  $consultaNoticias="SELECT * FROM noticias WHERE id_usuario=".$_SESSION['id_usuario']." ORDER BY fecha DESC LIMIT $cantidad OFFSET ".($pagina*$cantidad-$cantidad)."";
 
   $noticias = $conexion -> query($consultaNoticias); 
     
@@ -182,11 +185,8 @@ $_SESSION["paginaAnterior"]='noticias.php';
                 <small class="feature__label margin-bottom--xs">Etiquetas:&nbsp;<?= $noticia['etiquetas'] ?></small>
               </a>
             </div>
-            <div class="margin-top--sm">
-              <div class="">
-                <a href="noticias.php" class="boton btn--primario">Noticias</a>
-                <a href="noticia.php?idNoticia=<?= $noticia['id_noticia'] ?>" class="text--inherit">Ver&nbsp;m&aacute;s</a>
-              </div>
+            <div class="">
+              <a href="noticias.php" class="boton btn--delete">Eliminar</a>
             </div>
           </div>
         </div>
@@ -217,80 +217,23 @@ $_SESSION["paginaAnterior"]='noticias.php';
     <section class="feature feature--invert margin-bottom--xs">
       <div class="center">
         <div class="pagination2 ">
-          <a href="noticias.php?pagina=<?= ($pagina-1) ?>">&laquo;</a>
+          <a href="borrar.php?pagina=<?= ($pagina-1) ?>">&laquo;</a>
   <?php    
 
     $numero = ($pagina % 6 == 0 ? $pagina - (6 - 1) : 1 + (6*intdiv($pagina, 6)));
 
     for($i=$numero; $i< ($numero+6); $i++){ 
 
-      echo'<a href="noticias.php?pagina='.$i.'"'.( $i==$pagina ? 'class="active"': null).'>'.$i.'</a>';
+      echo'<a href="borrar.php?pagina='.$i.'"'.( $i==$pagina ? 'class="active"': null).'>'.$i.'</a>';
   
     }
 
   ?>
-          <a href="noticias.php?pagina=<?= ($pagina+1) ?>">&raquo;</a>
+          <a href="borrar.php?pagina=<?= ($pagina+1) ?>">&raquo;</a>
         </div>
       </div>
     </section>
   </div>
-
-  <?php 
-
-  if(isset($_SESSION["usuario"])) : ?>
-
-    <!-- Modal Trigger -->          
-    <div class="fixed-action-btn">
-      <a class="btn-floating btn-large "><i class="large material-icons">edit</i></a>
-      <ul>
-        <li><a class="btn-floating  modal-trigger" href="#modal1"><i class="material-icons">publish</i></a></li>
-        <li><a class="btn-floating  modal-trigger" href="borrar.php"><i class="material-icons">delete</i></a></li>
-      </ul>
-    </div>
-
-    <!-- Modal Structure -->
-    <div id="modal1" class="modal">
-      <div class="modal-header">
-        <h5 class="modal-title">Nueva noticia</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">x</button>
-      </div>
-      <form  enctype="multipart/form-data" id="formSubirNoticia">
-        <div class="modal-content row">
-          <div class="col s12 m6 l4">
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Titulo</label>
-              <input id="inputTitulo" type="text" class="form-control" name="titulo" placeholder="Titulo"  maxlength="100" required>
-            </div>
-            <div class="form-group">
-              <label for="exampleFormControlSelect1">Subtitulo</label>
-              <input id="inputSubtitulo" type="text" class="form-control" name="subtitulo" placeholder="Subtitulo" maxlength="100" required>
-            </div>
-            <div class="form-group">
-              <label for="exampleFormControlSelect1">Portada</label>
-              <div class="custom-file">
-                <input type="file" class="custom-file-input" name="imagen" id="imagen" lang="es" accept="image/*" required>
-                <label class="custom-file-label" id="select_file" placeholder="Seleccione una imagen"></label>
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="exampleFormControlSelect1">Etiquetas</label>
-              <input id="inputEtiquetas" type="text" class="form-control" name="etiquetas" placeholder="Ej:Gaming,Ps4,SmashBros" maxlength="50" required>
-            </div>
-          </div>
-          <div class="col s12 m6 l8">
-            <div class="form-group">
-              <label for="exampleFormControlTextarea1">Cuerpo de la noticia</label>
-              <textarea id="inputCuerpo" class="form-control s1 expand" name="cuerpo" style="resize: vertical; height: 290px;" placeholder="Ingrese texto.. " required></textarea>
-            </div>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <input type="submit" value="subir noticia" class="btn btn-secondary">
-        </div>
-      </form> 
-    </div>
-
-  <?php endif; ?>
   
   <script>
     //document.getElementsByTagName("html")[0].className += " js";
@@ -311,96 +254,6 @@ $_SESSION["paginaAnterior"]='noticias.php';
 
   </script>
 
-  <?php 
-  
-  if(isset($_SESSION["usuario"])) : ?>
-
-  <script type="text/javascript">
-
-  $(document).on('ready', function () {
-
-    $('[data-toggle="tooltip"]').tooltip();
-    $('.fixed-action-btn').floatingActionButton();
-    $('.modal').modal();
-    $('#select_file').html('Seleccione una imagen');
-
-    $('#formSubirNoticia').submit(function(e) {
-      e.preventDefault();
-      $("#loadingImg").show()
-
-      var frmData = new FormData();
-      frmData.append("titulo",$("#inputTitulo").val());
-      frmData.append("subtitulo",$("#inputSubtitulo").val());
-      frmData.append("cuerpo",$("#inputCuerpo").val());
-      frmData.append("etiquetas",$("#inputEtiquetas").val());
-      frmData.append("imagen",$('#imagen')[0].files[0]);
-      let date = new Date();
-      frmData.append("fecha",date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());   
-
-      $.ajax({
-        type: "POST",
-        url: 'assets/php/subir_noticia.php',
-        data: frmData,
-        processData: false,
-        contentType: false,
-          success: function(response){
-            //console.log(response);
-            var jsonData = JSON.parse(response);
-            if (jsonData.status.code == "1"){
-              
-              alert('Noticia Subida');
-              location.reload();
-              //window.history.back();
-            }else{
-              alert(jsonData.status.description);
-              $("#loadingImg").hide();
-            }
-          },
-          error: function() {
-            alert('Servidor no disponible');
-            $("#loadingImg").hide();
-          },
-      });
-    });
-
-    })
-
-    $('.close').on('click', function () {
-      $('.modal').modal('close');
-      $("#formSubirNoticia")[0].reset();
-      $('#select_file').html('Seleccione una imagen');
-    })
-
-    $('#imagen').on('change', function () {
-
-      if(this.files[0].size > 500000){
-       alert("Imagen demasiado pesada!");
-       this.value = null;
-      }else{
-        var filename = ($('#imagen').val()).substr(12); 
-        $('#select_file').html(filename);
-      }
-
-    })
-
-    //$("form").validate();
-
-    //  $('.btn-secondary').on('click', function () {
-    //   	 if($('form').valid()){
-    //      	$("textarea").animate({height: "260px"}); 
-    //   }else{
-    //   	$("textarea").animate({height: "389px"}); 
-    //   }
-    //   })
-
-    // jQuery.extend(jQuery.validator.messages, {
-    // required: "Campo obligatorio.",
-    // //email: "Por favor ingrese un e-mail valido",
-    // });
-
-  </script>
-
-  <?php endif; ?>
   <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
 
 </body>
